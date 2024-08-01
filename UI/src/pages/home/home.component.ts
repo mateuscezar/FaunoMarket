7ac5b756a34d34785e47003fab4500062af09451
mateuscezar <mateuscezarf@gmail.com>
2024-08-01 08:17:34 -0300

    feat(home) - header

--- UI/src/pages/home/home.component.ts
+++ UI/src/pages/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  value = 'Produto';
 }
